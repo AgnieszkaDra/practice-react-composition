7076bc8bae69d2b9afc9923735c40c124e46563b
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-06-01 13:08:05 +0200

    end task 03

--- 03/Product.js
+++ 03/Product.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 function Product(props) {
-    const {product, add, children} = props
+    const {product, add, bool, handleNameButton, remove} = props
     return (
     <>
        <h2>{product.name}</h2>
        <p>{product.price}</p>
        <button
-        onClick={()=> add(product)}
+        disabled={bool} 
+        onClick={()=> add ? add(product) : remove(product)}
             >
-        {'Add'}
+        {handleNameButton ? "Add" : "Remove"}
        </button>
     </>
     )
--- 03/app.js
+++ 03/app.js
@@ -17,28 +17,53 @@ class App extends React.Component {
             return {
                 cart: [...state.cart, product]
             }
-        }) 
+        })
     }
-    
+    removeProduct = (product) => {
+        this.setState(state => {
+            const newCart = state.cart.filter(element => element.id !== product.id);
+            return {cart: newCart}
+        })
+    }
+
     render() {
         return (
             <section>
                 <Category>
-                {data.map(product => {
-                           return (
-                                    <>
-                                   <Product   key={product.id} product={product} add={this.addProduct}></Product>
-                                    </>
-                                )
-                            })}
+                    {data.map(product => {
+                        if (this.state.cart.find(e => e.id === product.id)) {
+                            return (
+                                <Product
+                                    key={product.id}
+                                    product={product}
+                                    bool={true}
+                                    handleNameButton={false}
+                                    remove={this.removeProduct}
+                                />
+                            )
+                        } else {
+                            return (
+                                <Product
+                                    key={product.id}
+                                    product={product}
+                                    bool={false}
+                                    handleNameButton={true}
+                                    add={this.addProduct}
+                                />
+                            )
+                        }
+                    }
+                    )
+                    }
                 </Category>
                 <Cart>
-                {this.state.cart.map(product => {
+                    {this.state.cart.map(product => {
                         return (
-                            <Product 
-                                key={product.id} 
+                            <Product
+                                key={product.id}
                                 product={product}
-                                
+                                bool={false}
+                                remove={this.removeProduct}
                             />
                         )
                     })}
